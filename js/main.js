2a359c9493e68c7fe2b50c5531e08c06a183e4b0
LDH <luvya8055@naver.com>
2023-04-10 15:38:25 +0900

    반응형 레이아웃 수정

--- js/main.js
+++ js/main.js
@@ -1,14 +1,60 @@
 $(function () {
-  $('.gnb').hover(
-    function () {
-      $('.header').css('background', 'rgba(30, 30, 30, 0.9)').css('transition', '0s');
-    },
-    function () {
-      setTimeout(function () {
-        $('.header').css('transition', '0.5s').css('background', 'transparent');
-      }, 500);
+  // 헤더 호버 이벤트
+  const handleHeaderHover = () => {
+    $('.gnb').hover(
+      function () {
+        $('.header').css('background', 'rgba(30, 30, 30, 0.9)').css('transition', '0s');
+      },
+      function () {
+        setTimeout(function () {
+          $('.header').css('background', 'transparent').css('transition', '0.5s');
+        }, 500);
+      }
+    );
+  };
+
+  const cancelHandleHeaderHover = () => {
+    $('.gnb').hover(
+      function () {
+        $('.header').css('background', 'inherit').css('transition', '0s');
+      },
+      function () {
+        setTimeout(function () {
+          $('.header').css('background', 'inherit').css('transition', '0s');
+        }, 500);
+      }
+    );
+  };
+
+  const handleGnbClick = () => {
+    $('.menu').click(function () {
+      $('.sub_menu', this).toggleClass('on');
+    });
+  };
+
+  if ($(window).width() > 1024) {
+    // 1024px 이상 display load시
+    handleHeaderHover();
+  } else {
+    cancelHandleHeaderHover();
+    handleGnbClick();
+  }
+
+  $(window).resize(function () {
+    if ($(window).width() > 1024) {
+      // 1024xp 이상 display resize
+      handleHeaderHover();
+    } else {
+      cancelHandleHeaderHover();
+      handleGnbClick();
     }
-  );
+  });
+
+  // 모바일 메뉴버튼 클릭 이벤트
+
+  $('.m_open').click(function () {
+    $('.hd_down').toggleClass('on');
+  });
 
   $('main').fullpage({
     navigation: true,
